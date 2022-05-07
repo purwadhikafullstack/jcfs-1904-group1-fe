e811c44e29fddef1da63809bdb89996c82fe204e
Michael Gary Ringgas <michaelsiahaan18@yahoo.com>
2022-05-08 00:50:07 +0700

    update stock per variant, checkout, cannot shop before login

--- src/pages/Carts/index.js
+++ src/pages/Carts/index.js
@@ -44,6 +44,7 @@ function Carts() {
       alert(error);
     }
   };
+  console.log(carts);
 
   useEffect(() => {
     fetchCarts();
@@ -223,55 +224,63 @@ function Carts() {
             Total Price
           </Typography>
         </Box>
-        {renderCarts()}
+        {carts.length ? (
+          renderCarts()
+        ) : (
+          <Typography>No products in cart!</Typography>
+        )}
       </Paper>
-      <Paper
-        sx={{
-          width: "15%",
-          height: 130,
-          backgroundColor: "white",
-          marginBottom: 10,
-          marginTop: 10,
-          borderRadius: 3,
-          boxShadow: 3,
-        }}
-      >
-        <Box display="flex" justifyContent="end" marginRight="12px">
-          <Box
-            display="flex"
-            justifyContent="space-between"
-            alignContent="center"
-          >
-            <Box>
-              <Typography mr="12px">Sub Total : </Typography>
-              <Typography mr="12px">
-                PPn {priceState.ppnObat * 100}%:
-              </Typography>
-              <Typography mr="12px">Total : </Typography>
-            </Box>
-            <Box>
-              <Typography>
-                Rp {priceState.total.toLocaleString("id")}
-              </Typography>
+      {carts.length ? (
+        <Paper
+          sx={{
+            width: "15%",
+            height: 130,
+            backgroundColor: "white",
+            marginBottom: 10,
+            marginTop: 10,
+            borderRadius: 3,
+            boxShadow: 3,
+          }}
+        >
+          <Box display="flex" justifyContent="end" marginRight="12px">
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignContent="center"
+            >
+              <Box>
+                <Typography mr="12px">Sub Total : </Typography>
+                <Typography mr="12px">
+                  PPn {priceState.ppnObat * 100}%:
+                </Typography>
+                <Typography mr="12px">Total : </Typography>
+              </Box>
+              <Box>
+                <Typography>
+                  Rp {priceState.total.toLocaleString("id")}
+                </Typography>
 
-              <Typography>Rp {priceState.tax.toLocaleString("id")}</Typography>
+                <Typography>
+                  Rp {priceState.tax.toLocaleString("id")}
+                </Typography>
 
-              <Typography>
-                Rp {priceState.totalAfterTax.toLocaleString("id")}
-              </Typography>
+                <Typography>
+                  Rp {priceState.totalAfterTax.toLocaleString("id")}
+                </Typography>
+              </Box>
             </Box>
           </Box>
-        </Box>
-        <Box display="flex" justifyContent="end">
-          <Button
-            variant="contained"
-            sx={{ margin: "12px" }}
-            onClick={onCheckoutClick}
-          >
-            Checkout
-          </Button>
-        </Box>
-      </Paper>
+          <Box display="flex" justifyContent="end">
+            <Button
+              variant="contained"
+              sx={{ margin: "12px" }}
+              onClick={onCheckoutClick}
+            >
+              Checkout
+            </Button>
+          </Box>
+        </Paper>
+      ) : null}
     </Box>
   );
 }
